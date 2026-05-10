658e041322303025beb1200ea4ba4c006a95fb94
Geut Galai <59103837+Geutg@users.noreply.github.com>
2026-05-10 13:59:34 +0300

    moving doc folder to root
    easier to deploy github page.
    make sure it isnt included when building for CRAN.

--- website/website_source/make_site.R
+++ website/website_source/make_site.R
@@ -5,7 +5,7 @@ remotes::install_github("rlesur/klippy")
 rmarkdown::render_site(input = './website/website_source')
 
 # Remove unnecesary files
-unlink('./website/docs/', recursive = TRUE, force = FALSE) # delete old one
-file.copy('./website/website_source/docs/', './website', recursive = TRUE)
+unlink('./docs/', recursive = TRUE, force = FALSE) # delete old one
+file.copy('./website/website_source/docs/', '.', recursive = TRUE)
 unlink('./website/website_source/analysis_example_files/', recursive = TRUE, force = FALSE)
 unlink('./website/website_source/docs/', recursive = TRUE, force = FALSE)
